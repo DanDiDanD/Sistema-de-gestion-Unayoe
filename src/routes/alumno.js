bca0e07dde2d8bb26dbeecefb31f41c3df6d682a
Daniel <pfbarrett98@gmail.com>
2019-11-07 11:43:55 -0500

    filtros hechos

--- src/routes/alumno.js
+++ src/routes/alumno.js
@@ -12,6 +12,7 @@ router.get('/listaAlumnos',isLoggedIn, async (req, res) => {
      res.render('alumno/listaAlumnos', {alumnos});
 })
 
+
 router.get('/perfilAlumno/:idAlumno', isLoggedIn, async (req, res) => {
      const {idAlumno} =req.params;
      const [alumnoCompleto] = await pool.query('CALL sp_alumnoCompleto(?)', [idAlumno]); //a de alumnos
@@ -25,6 +26,122 @@ router.post('/perfilAlumno/:idAlumno', isLoggedIn, async (req, res) => {
      res.redirect('/alumno/listaAlumnos');
 })
 
+router.post('/listaAlumnos',isLoggedIn, async (req, res) => {
+     let {nombre,
+               escuela,
+               observacion,
+               desaprobados,
+               ponderado} = req.body;
+     
+     if(escuela == 1){
+          escuela = 'sis';
+     }
+     else if(escuela == 2){
+          escuela = 'sw';
+     }
+
+     if(observacion == 1){
+          observacion = 'ansiedad';
+     }
+     else if(observacion == 2){
+          observacion = 'depresion';
+     }
+
+     let query = `select  a.idAlumno as codigo 
+                         ,a.alumnoApellidoPaterno as ApePaterno
+                         ,a.alumnoApellidoMaterno as ApeMaterno
+                         ,a.alumnoNombre as Nombre
+                         ,a.alumnoEscuela as Escuela
+                         ,s.sumSituacionAcademica as SitAcademica
+                         ,s.sumCantDesaprobados as NumDesaprobados
+                         ,s.sumPromedioPonderado as Ponderado
+                         ,o.ocObservacionPsicologica as Observacion
+                         from  alumno a
+                         left join SUM s 
+                         on s.Alumno_idAlumno=a.idAlumno
+                         left join ocaClinica o 
+                         on o.Alumno_idAlumno= a.idAlumno `;
+
+     let bandera = false;
+     let condiciones = '';
+
+     if(nombre.length != 0){
+          condiciones += `(a.alumnoNombre like '${nombre}%'
+                    or a.alumnoApellidoPaterno like '${nombre}%'  
+                    or a.alumnoApellidoMaterno like '${nombre}%' 
+                    or a.alumnoDni like '${nombre}%' 
+                    or a.idAlumno like'${nombre}%') ` ;
+          bandera = true;
+     }
+     if(escuela!=0){
+          if(bandera == true){
+               condiciones += 'AND ';
+          }
+          condiciones += `a.alumnoEscuela = '${escuela}' `;
+          bandera = true;
+     }
+     if(observacion!=0){
+          if(bandera == true){
+               condiciones += 'AND ';
+          }
+          condiciones += `o.ocObservacionPsicologica like '${observacion}' `;
+          bandera = true;
+     }
+     if(desaprobados.length!=0){
+          if(bandera == true){
+               condiciones += 'AND ';
+          }
+          condiciones += `s.sumCantDesaprobados >= '${desaprobados}' `;
+          bandera = true;
+     }
+     if(ponderado.length!=0){
+          if(bandera == true){
+               condiciones += 'AND ';
+          }
+          condiciones += `s.sumPromedioPonderado <= '${ponderado}' `;
+          bandera = true;
+     }
+
+     if(bandera==true){
+          query+='WHERE ';
+          query+=condiciones;
+     }
+     query += `order by a.idAlumno desc;`;
+                              
+     
+     console.log(query);
+     
+     const a = await pool.query(query);
+     console.log(a);
+     
+
+     if(escuela == 'sis'){
+          escuela = 1;
+     }
+     else if(escuela == 'sw'){
+          escuela = 2;
+     }
+
+     if(observacion == 'ansiedad'){
+          observacion = 1;
+     }
+     else if(observacion == 'depresion'){
+          observacion = 2;
+     }
+
+
+     const filtros = {
+          nombre,
+          escuela: parseInt(escuela),
+          observacion,
+          desaprobados,
+          ponderado
+     };
+     console.log(filtros);
+     
+     res.render('alumno/listaAlumnosFiltrada', {alumnos: a, filtros: filtros});    
+     
+})
 
 router.get('/editarPerfilAlumno/:idAlumno', isLoggedIn, async (req, res) => {
      const {idAlumno} = req.params;
